1b9add2e36c3874436903d5b6c60284ce33b64ed
VinMeh1001 <equityyog@gmail.com>
2024-12-17 22:55:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import CollegeSearch from 'topperteacher/blob/VinMeh1001-patch-1/src/components/CollegeSearch'; // Import your CollegeSearch component
+import CollegeSearch from 'components/CollegeSearch'; // Import your CollegeSearch component
 
 const queryClient = new QueryClient();
 
@@ -16,8 +16,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* Add CollegeSearch route if needed */}
-          {/* <Route path="/search" element={<CollegeSearch onSelect={handleSelect} />} /> */}
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
