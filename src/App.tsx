b25d6f9b5d90e5eee26664a5ce4c2eba495b9da1
VinMeh1001 <equityyog@gmail.com>
2024-12-17 22:09:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import CollegeSearch from 'topperteacher/src/components/CollegeSearch'; // Import your CollegeSearch component
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          {/* Add CollegeSearch route if needed */}
+          {/* <Route path="/search" element={<CollegeSearch onSelect={handleSelect} />} /> */}
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
